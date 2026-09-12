deba2c7ba8c6f5697b15be2c9b19ea78278faf2a
superstilist <samokisukegshdhhr@gmail.com>
2026-09-12 17:52:11 +0200

    .

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import crud
 DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
