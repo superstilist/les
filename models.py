b4cbda6c8121f9974074f7085509028b6e4f6982
superstilist <samokisukegshdhhr@gmail.com>
2026-09-12 17:48:08 +0200

    .

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
-from typing import Optional
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./users.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 
-class User(BaseModel):
-    id: Optional[int] = None
-    username: str
-    email: str
+class UserModel(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
